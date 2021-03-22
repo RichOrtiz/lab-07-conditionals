260cd10135d318459e634e087dd8129ef0f894bd
Richard Ortiz <ortizrichard2@gmail.com>
2021-03-22 16:20:16 -0400

    completed oddcalc html file and cake python file

---
+++ cake.py
@@ -0,0 +1,12 @@
+response = input("Do you want cake? yes or no? ")
+
+
+if response == "yes" or response == "YES" or response == "y":
+    print("Have some cake")
+        
+elif response == "no" or response == "NO" or response == "n":
+    print("No cake for you")
+
+else:
+    print("Bruh, can you just follow directions")
+        
