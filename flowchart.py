9af7f46fb7cdd56e242a81d8cfa5185b90f31a55
Richard Ortiz <ortizrichard2@gmail.com>
2021-03-22 21:50:40 -0400

    added some spaces for fun

--- flowchart.py
+++ flowchart.py
@@ -8,11 +8,11 @@ if myVar == "yes":
     print("Then use duct tape!!!")
   else:
     print("Answer my question! You didn't type yes or no.")
-
+    
 elif myVar == "no":
   myNextVar = input("What is your answer to my 2nd question? (yes/no) ")
   if myNextVar == "yes":
-    print("Then Use WD-40, you goofball!")
+    print("Then Use WD-40, you goofball!")           
   elif myNextVar == "no":
     print("No problem")
   else:
